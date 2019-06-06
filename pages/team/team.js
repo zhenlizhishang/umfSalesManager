f13ad876f40938f47d67d8ed8abe54677437ab82
unknown <unknown@example.com>
2019-06-06 18:34:05 +0800

    1
    1

---
+++ pages/team/team.js
@@ -0,0 +1,29 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    teams: [
+      {
+        imgUrl:'/pages/img/team/team.png',
+        username:'刘佳昱',
+        role:'管理员',
+        phone:'17601023786',
+        reqUrl:''
+      }, {
+        imgUrl: '/pages/img/team/team.png',
+        username: '魏超',
+        role: '管理员',
+        phone: '15754314122',
+        reqUrl: ''
+      }, {
+        imgUrl: '/pages/img/team/team.png',
+        username: '李雪敏',
+        role: '运营',
+        phone: '15754314122',
+        reqUrl: ''
+      }
+    ]
+  }
+})
